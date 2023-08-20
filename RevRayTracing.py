7ad612c7e9d8610c88f2bd8d1fa3445c7b799b5e
wialb <william.albert124@outlook.com>
2023-08-20 14:16:41 -0400

    Rays with Z param working

--- RevRayTracing.py
+++ RevRayTracing.py
@@ -36,18 +36,24 @@ def revRayTracing(ply_path_voxels, ply_path_normals):
     voxels = np.c_[x, y, z, lbl, nx, ny, nz]
     del x, y, z, lbl, nx, ny, nz
     
-    dist_step = 0.1                        # Distance for only one move
+    dist_step = 0.4                        # Distance for only one move
     dist_at_angle_0 = 13                    # Meters
     angle_step = np.deg2rad(0.5)            # 0.5 degrees
-    angle_max = np.deg2rad(60)              # 120 degrees 
+    angle_max = np.deg2rad(30)              # 120 degrees 
     
     nb_angle_steps = np.ceil((angle_max*2) / angle_step).astype(np.int32)
-    
     idx_facade = np.where(voxels[:,4:] != (0.,0.,0.))[0].astype(np.int32)   # Get indices of all facade building voxels (where normal != 0)
+    
+    idx_flat = np.where(voxels[:,3] == 1)[0].astype(np.int32)   # Get indices of all flat voxels to get the min and max
+    z_min = np.min(voxels[idx_flat][:,2])
+    z_max = np.max(voxels[idx_flat][:,2])
+    z_mean = np.mean(voxels[idx_flat][:,2])
+    nb_dist_steps_verti = np.ceil((z_max - z_min)/0.5)
+    
     lst = []
     for idx in np.arange(idx_facade.shape[0]):
-        input("Press Enter to continue...")
-        idx = 100
+        idx = 1000
+        # input("Press Enter to continue...")
         if voxels[idx_facade[idx]][3] != 6:
             continue
         else:
@@ -55,22 +61,33 @@ def revRayTracing(ply_path_voxels, ply_path_normals):
             starting_vxl_normal = voxels[idx_facade[idx]][4:]
             for idx_current_ray in np.arange(nb_angle_steps):
                 dist_tot_to_travel = dist_at_angle_0 / cos(angle_max - angle_step*idx_current_ray)
-                nb_dist_steps = dist_tot_to_travel / dist_step         # Number of time we have to move forward in the current ray
+                nb_dist_steps = np.ceil(dist_tot_to_travel / dist_step)         # Number of time we have to move forward in the current ray
                 
                 for idx_dist_step in np.arange(1, nb_dist_steps+1):
                     angle_x = np.arccos(np.dot(starting_vxl_normal, np.array([1, 0, 0])))       # Rad
                     angle_y = np.arccos(np.dot(starting_vxl_normal, np.array([0, 1, 0])))       # Rad
                     angle_z = np.arccos(np.dot(starting_vxl_normal, np.array([0, 0, 1])))       # Rad
-                    point_ray_interm_x = starting_vxl_center[0] + cos(angle_x - (angle_max - angle_step*idx_current_ray)) * (dist_step * idx_dist_step)
-                    point_ray_interm_y = starting_vxl_center[1] + sin(angle_y - (angle_max - angle_step*idx_current_ray)) * (dist_step * idx_dist_step)
                     
-                    lst.append([point_ray_interm_x, point_ray_interm_y])
+                    adjustment_x = cos(angle_x - (angle_max - angle_step*idx_current_ray)) * (dist_step * idx_dist_step)
+                    adjustment_y = -sin(angle_y - (angle_max - angle_step*idx_current_ray)) * (dist_step * idx_dist_step)
+                    
+                    point_ray_interm_x = starting_vxl_center[0] + adjustment_x
+                    point_ray_interm_y = starting_vxl_center[1] + adjustment_y
+                    
+                    for idx_dist_step_verti in np.arange(1, nb_dist_steps_verti+1):
+                        angle = np.arctan2((abs(starting_vxl_center[2]-z_mean)-idx_dist_step_verti*dist_step), dist_tot_to_travel)
+                        corr_z = np.sqrt(adjustment_x**2 + adjustment_y**2) * tan(angle)
+                        point_ray_interm_z = starting_vxl_center[2] - corr_z
+                    
+                        lst.append([point_ray_interm_x, point_ray_interm_y, point_ray_interm_z])
+                    
+        break
                 
         
     # voxels[idx_building_all[3]] PAS TOUS LES VOXELS BUILDING QUI ONT UNE NORMAL car seulement les facades en ont une
         
     num_steps = dist_at_angle_0 * config.voxel_size
-    ost.write_ply("grosTEST.ply", np.array(lst), ['x','y'])
+    ost.write_ply("grosTEST.ply", np.array(lst), ['x','y','z'])
     
     
     
